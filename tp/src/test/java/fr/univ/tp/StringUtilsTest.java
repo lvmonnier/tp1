72f42ebe1e6c42ad0764fb6d63781270162af210
lm432183 <lm432183@MI103-02>
2017-04-05 17:36:19 +0200

    ajout de méthode de test chaine vide, single, mauvaistype

--- tp/src/test/java/fr/univ/tp/StringUtilsTest.java
+++ tp/src/test/java/fr/univ/tp/StringUtilsTest.java
@@ -2,17 +2,38 @@ package fr.univ.tp;
 
 import java.util.logging.Logger;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 public class StringUtilsTest {
 	private static Logger log = Logger.getLogger(MyFirstTest.class.getSimpleName());
 	private StringUtils su = new StringUtils();
 	
+	@Rule
+	public ExpectedException exc = ExpectedException.none();
+	
 	@Test
-	public void reserverStringTest() {
-		log.info("Test de la méthode reverseStringTest");
+	public void reverseStringTestCasNominal() {
+		log.info("Test de la méthode reverseStringTest pour la chaine \"test\"");
 		Assert.assertEquals("tset", su.reverseString("test"));
+	}
+	
+	@Test
+	public void reverseStringTestCasVide() {
+		log.info("Test de la méthode reverseStringTest pour une chaine vide");
+		Assert.assertEquals("", su.reverseString(""));
+	}
+	
+	@Test
+	public void reverseStringTestCasSingle() {
+		log.info("Test de la méthode reverseStringTest pour un seul caractère");
+		Assert.assertEquals("a", su.reverseString("a"));
+	}
+	
+	@Test
+	public void reverseStringTestCasNonString() {
+		log.info("Test de la méthode reverseStringTest pour un mauvais argument (non string)");
+		exc.expect(IllegalArgumentException.class);
 	}
 
 }
